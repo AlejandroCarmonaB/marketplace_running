d9742a4b6a655f4ae8073b0d6cef6dce2cf4bb0d
AlejandroCarmonaB <Krmona17@hotmail.com>
2026-04-24 21:45:31 +0200

    Actualizacion 11
    Preparar despliegue con Neon y Cloudinary

--- marketplace-running/src/config/db.js
+++ marketplace-running/src/config/db.js
@@ -3,12 +3,26 @@ require("dotenv").config();
 
 // Creamos un pool de conexiones a PostgreSQL.
 // Esto permite reutilizar conexiones y trabajar mejor que abriendo una nueva cada vez.
+
+/*
+Esta parte del código se ha editado para usar DATABASE_URL, que es una forma común de configurar la conexión a la base de datos en entornos como Heroku o Neon. Si estás usando un entorno local, 
+puedes seguir usando las variables individuales como DB_HOST, DB_PORT, etc.
+
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+});
+*/
+
+//Esta parte es para cuando se despliegue en un entorno que requiere SSL, como Neon. Si estás trabajando localmente sin SSL, puedes usar la configuración anterior.
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 // Exportamos el pool para poder usarlo en otras partes del proyecto.
